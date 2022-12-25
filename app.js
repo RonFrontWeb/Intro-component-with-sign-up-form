ab2847e00fb3ca3dbc417c92053e03effb6b6b6a
ronny christensen <6382104@rts-365.dk>
2022-12-25 15:34:10 +0000

    mobile first

--- app.js
+++ app.js
@@ -1,7 +1,58 @@
 function App() {
     return (
         <div>
-            Lets begin 
+            <Grid/>
+        </div>
+    )
+}
+function Grid() {
+    return (
+        <div className="grid" >
+            <TextArea/>
+            <TryFreeBlock/>
+            <ContactForm/>
+        </div>
+    )
+}
+function TextArea() {
+    return (
+        <div className="text__container" >
+            <h1 className="text__headline">Learn to code by watching others</h1>
+            <p className="text">See how experienced developers solve problems in real-time. Watching scripted tutorials is great, 
+                but understanding how developers think is invaluable.
+            </p>
+        </div>
+    )
+}
+
+function TryFreeBlock() {
+    return (
+        <div className="try" >
+            <p><span className="try__highlight">Try it free 7 days</span> then</p>
+            <p>$20/mo. thereafter</p>
+        </div>
+    )
+}
+
+function ContactForm() {
+    return (
+        <div className="form__container" >
+            <form className="form">
+                <label htmlFor="">
+                    <input className="form__input" type="text" placeholder="First Name" />
+                </label>
+                <label htmlFor="">
+                    <input className="form__input" type="text" placeholder="Last Name"  />
+                </label>
+                <label htmlFor="">
+                    <input className="form__input" type="email" placeholder="Email Adress" />
+                </label>
+                <label htmlFor="">
+                    <input className="form__input" type="password" placeholder="Password" />
+                </label>
+                <input className="form__button form__input" type="submit" value="Claim your free trail" />
+                <p className="form__fineprint">By clicking the button, you are agreeing to our <span className="form__fineprint_highlight">Terms and Services</span></p>
+            </form>
         </div>
     )
 }
