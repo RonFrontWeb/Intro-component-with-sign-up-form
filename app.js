7f52b9c58206b100417967e15b2d61929bdd677f
ronny christensen <6382104@rts-365.dk>
2022-12-27 20:47:24 +0000

    form validation

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const { useState } = React;
+
 function App() {
     return (
         <div>
@@ -34,23 +36,72 @@ function TryFreeBlock() {
     )
 }
 
+function isValidEmail(email) {
+    return /\S+@\S+\.\S+/.test(email);
+  }
+
 function ContactForm() {
+
+    const [error , setError] = useState("")
+
+    
+
+    const handleChange = (key) => {
+        setError(prev => ({...prev ,[key]:""}));
+    }
+
+    const handleSubmit =(event) => {
+
+        const err = {}
+
+        console.log("submitted")
+        event.preventDefault();
+
+        if (event.target.firstName.value === "") {
+            err.firstName ="first name need to be filled out"
+        }
+        if (event.target.lastName.value === "") {
+            err.lastName ="last name need to be filled out"
+
+        }
+        if (!isValidEmail(event.target.email.value)) {
+            err.email = "besked"
+          }
+
+        if (event.target.email.value === "") {
+            err.email ="Email need to be filled out"
+
+        }
+        if (event.target.password.value === "") {
+            err.password ="password need to be filled out"
+
+        }
+        if (Object.keys(err).length !== 0) {
+            setError(err);
+          }
+    }
+    
+
     return (
         <div className="form__container" >
-            <form className="form">
+            <form noValidate className="form" onSubmit={handleSubmit}>
                 <label htmlFor="">
-                    <input className="form__input" type="text" placeholder="First Name" />
+                    <input name="firstName" onChange={() => handleChange("firstName")} className="form__input" type="text" placeholder="First Name"/>
+                 <p className="error">{error.firstName}</p>
                 </label>
                 <label htmlFor="">
-                    <input className="form__input" type="text" placeholder="Last Name"  />
+                    <input name="lastName" className="form__input" onChange={() => handleChange("lastName")} type="text" placeholder="Last Name"  />
+                    <p className="error">{error.lastName}</p>
                 </label>
                 <label htmlFor="">
-                    <input className="form__input" type="email" placeholder="Email Adress" />
+                    <input name="email" className="form__input"  onChange={() => handleChange("email")} type="email" placeholder="Email Adress" />
+                    <p className="error">{error.email}</p>
                 </label>
                 <label htmlFor="">
-                    <input className="form__input" type="password" placeholder="Password" />
+                    <input name="password" className="form__input" onChange={() => handleChange("password")} type="password" placeholder="Password" />
+                    <p className="error">{error.password}</p>
                 </label>
-                <input className="form__button form__input" type="submit" value="Claim your free trail" />
+                <input name="submit" className="form__button form__input"  type="submit" value="Claim your free trail" />
                 <p className="form__fineprint">By clicking the button, you are agreeing to our <span className="form__fineprint_highlight">Terms and Services</span></p>
             </form>
         </div>
